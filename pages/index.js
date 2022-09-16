30d1e598e6ff86ea55842f01672213189a341ca2
Vicky-D <vijaymasiwal.uk@gmail.com>
2022-09-16 16:56:14 +0530

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -7,11 +7,11 @@ export default function Home() {
     <div className="container">
       <Head>
         <title>Next.js Starter!</title>
-    <meta name="title" content="Business Energy Solutions | Shell Energy" data-react-helmet="true">
-    <meta name="title" property="og:title" content="Business Energy Solutions | Shell Energy" data-react-helmet="true">
-    <meta name="description" content="Shell Energy can help you build momentum by breaking down the complexities of your energy transition." data-react-helmet="true">
-    <meta name="description" property="og:description" content="Shell Energy can help you build momentum by breaking down the complexities of your energy transition." data-react-helmet="true">
-    <meta name="image" property="og:image" content="https://images.ctfassets.net/1n54v69mwqrd/2p7JxG9jdDmYv3q3NROBfv/6b107b898eef7b0b1b497b7c04ce08a8/banner.jpg" data-react-helmet="true">
+    <meta name="title" content="Business Energy Solutions | Shell Energy" data-react-helmet="true"/>
+    <meta name="title" property="og:title" content="Business Energy Solutions | Shell Energy" data-react-helmet="true"/>
+    <meta name="description" content="Shell Energy can help you build momentum by breaking down the complexities of your energy transition." data-react-helmet="true"/>
+    <meta name="description" property="og:description" content="Shell Energy can help you build momentum by breaking down the complexities of your energy transition." data-react-helmet="true"/>
+    <meta name="image" property="og:image" content="https://images.ctfassets.net/1n54v69mwqrd/2p7JxG9jdDmYv3q3NROBfv/6b107b898eef7b0b1b497b7c04ce08a8/banner.jpg" data-react-helmet="true"/>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
